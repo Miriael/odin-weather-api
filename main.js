d6941e1c7e2366735735642dcfc8770687e932c6
Miriael <ravafiel@protonmail.com>
2023-02-16 02:08:53 +0100

    Add a function to fetch weather data from coordinates

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ async function getLocation(city, countryCode, stateCode) {
     try {
       let url = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},${stateCode},${countryCode}&limit=1&appid=6538885ee061064bc99332441ad15f2b`);
       let result = await url.json();
-      console.log([result[0].lat, result[0].lon]);
       return [result[0].lat, result[0].lon];
     } catch (err) {
       alert(err);
@@ -15,9 +14,18 @@ async function getLocation(city, countryCode, stateCode) {
   }else try {
       let url = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},${countryCode}&limit=1&appid=6538885ee061064bc99332441ad15f2b`);
       let result = await url.json();
-      console.log([result[0].lat, result[0].lon]);
       return [result[0].lat, result[0].lon];
     } catch (err) {
       alert(err);
     };
 };
+
+async function getWeatherData(coords) {
+  try {
+    let url = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${coords[0]}&lon=${coords[1]}&units=metric&appid=6538885ee061064bc99332441ad15f2b`);
+    let result = await url.json();
+    return result.main.temp;
+  } catch (err) {
+    console.log('error');
+  }
+}
