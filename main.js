27fb2cd39e656fb2e8384a519bc88eb02eee6d37
Miriael <ravafiel@protonmail.com>
2023-02-17 06:21:48 +0100

    Add a list of countries\states and a way to access them

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
 import './style.css'
 //6538885ee061064bc99332441ad15f2b
 
+const countryCodes = await (await fetch('./public/world.json')).json();
+const stateCodes = await (await fetch('./public/subdivisions.json')).json();
+
+function getAllCountries(){
+  for(let element of countryCodes) {
+    return(element.name)
+  }
+}
+
+function getAllUSStates(){
+  for(let element of stateCodes) {
+    if(element.country === 'US') {
+      return(element.name)
+    }
+  }
+}
+
+getAllCountries()
+getAllUSStates()
+
+function getCountryCode(country) {
+  return countryCodes.find((a) => {return a.name === country}).alpha2;
+}
+
+function getStateCode(state) {
+  return stateCodes.find((a) => {return a.name === state}).code;
+}
+
 async function getLocation(city, countryCode, stateCode) {
   console.log(countryCode)
   if(countryCode == 'US') {
