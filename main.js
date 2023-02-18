201b5bf67c456ca16cdbc159f85dffad00dabc22
Miriael <ravafiel@protonmail.com>
2023-02-18 12:20:12 +0100

    Add basic interface

--- main.js
+++ main.js
@@ -1,20 +1,35 @@
 import './style.css'
 //6538885ee061064bc99332441ad15f2b
 
-const countryCodes = await (await fetch('./public/world.json')).json();
-const stateCodes = await (await fetch('./public/subdivisions.json')).json();
+const countryCodes = await (await fetch('/world.json')).json();
+const stateCodes = await (await fetch('/subdivisions.json')).json();
+const stateDropdown = document.querySelector('#state');
+const countryDropdown = document.querySelector('#country');
+
+countryDropdown.addEventListener('input', () => {
+  console.log(countryDropdown.value)
+  if(countryDropdown.value === 'United States of America'){
+    stateDropdown.style['visibility'] = 'visible';
+  }else {
+    stateDropdown.style['visibility'] = 'hidden';
+  }
+})
 
 function getAllCountries(){
   for(let element of countryCodes) {
-    return(element.name)
+    let country = document.createElement('option');
+    country.innerHTML = element.name;
+    country.setAttribute('value', element.name);
+    countryDropdown.appendChild(country);
   }
 }
 
 function getAllUSStates(){
-  for(let element of stateCodes) {
-    if(element.country === 'US') {
-      return(element.name)
-    }
+  for(let element of stateCodes.filter((a) => {if(a.country != 'US'){return false}return true})) {
+    let state = document.createElement('option');
+    state.innerHTML = element.name;
+    state.setAttribute('value', element.name);
+    stateDropdown.appendChild(state);
   }
 }
 
@@ -30,7 +45,6 @@ function getStateCode(state) {
 }
 
 async function getLocation(city, countryCode, stateCode) {
-  console.log(countryCode)
   if(countryCode == 'US') {
     try {
       let url = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},${stateCode},${countryCode}&limit=1&appid=6538885ee061064bc99332441ad15f2b`);
@@ -48,12 +62,45 @@ async function getLocation(city, countryCode, stateCode) {
     };
 };
 
+const weatherpanel = document.querySelector('.weather-panel');
+
+//Set up loop via index with steps of 2, and add weather data alternatingly between two divs in two columns via grid to fit maybe?
+
+function ass () {
+    for(let i = 0; i < 8; i++){
+      weatherpanel.innerHTML = weatherpanel.innerHTML + 'Time - 12:00<br>';
+      weatherpanel.innerHTML = weatherpanel.innerHTML + 'Temp ass<br>';
+      weatherpanel.innerHTML = weatherpanel.innerHTML + 'Temp butt<br>';
+      weatherpanel.innerHTML = weatherpanel.innerHTML + 'Butt - butt<br>'
+    }
+  }
+
+ass();
+
+
 async function getWeatherData(coords) {
   try {
-    let url = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${coords[0]}&lon=${coords[1]}&units=metric&appid=6538885ee061064bc99332441ad15f2b`);
+    let url = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${coords[0]}&lon=${coords[1]}&units=metric&appid=6538885ee061064bc99332441ad15f2b`);
     let result = await url.json();
-    return result.main.temp;
+    console.log(result.list);
+    let arrayOfDays = [[],[],[],[],[]];
+    for(let i = 0; i < 5; i++){
+      arrayOfDays[i] = result.list.slice(i, i+8);
+    }
+    for(let day of arrayOfDays){
+      console.log(day);
+      for(let interval of day){
+        weatherpanel.innerHTML = weatherpanel.innerHTML + interval.dt_txt.slice(11, -1).slice(0, 5);
+        console.log(interval.dt_txt.slice(11, -1).slice(0, 5));
+        console.log(interval.main.temp_min);
+        console.log(interval.main.temp_max);
+        console.log(interval.weather[0].description);
+      }
+    }
+    console.log(arrayOfDays);
   } catch (err) {
     console.log('error');
   }
 }
+
+//getWeatherData(await getLocation('London','GB'))
