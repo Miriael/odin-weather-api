1a5fb21fe9dcdfc1379786b5d3439fd552052767
Miriael <ravafiel@protonmail.com>
2023-02-23 17:14:47 +0100

    Improve interface styling

--- modules/display.js
+++ modules/display.js
@@ -1,7 +1,7 @@
 async function displayData(currentResult, arrayOfDays, units) {
   document.querySelector('.weather-container').innerHTML = '';
   let currentWeatherContainer = document.createElement('div');
-  currentWeatherContainer.classList.add('weather-panel');
+  currentWeatherContainer.classList.add('weather-panel', 'current');
   let currentTemp = document.createElement('p');
   currentTemp.classList.add('temperature');
   currentTemp.textContent = 'Current temperature: ' + currentResult.main.temp + units;
@@ -13,21 +13,35 @@ async function displayData(currentResult, arrayOfDays, units) {
   for(let day of arrayOfDays){
     let dayContainer = document.createElement('div');
     dayContainer.classList.add('weather-panel');
-    for(let interval of day){
+    let spacerTop = document.createElement('div');
+    spacerTop.style.gridColumnStart = '1';
+    spacerTop.style.gridColumnEnd = '3';
+    dayContainer.appendChild(spacerTop)
+    for(let i = 0; i < day.length; i++){
+      let intervalContainer = document.createElement('div');
+      intervalContainer.classList.add('interval-container');
+      dayContainer.appendChild(intervalContainer);
       let date = document.createElement('p');
-      date.textContent = 'Date: ' + interval.dt_txt;
-      dayContainer.appendChild(date);
+      date.textContent = 'Date: ' + day[i].dt_txt;
+      intervalContainer.appendChild(date);
       let tempMax = document.createElement('p');
       tempMax.classList.add('temperature');
-      tempMax.textContent = 'Max temperature: ' + interval.main.temp_max + units;
-      dayContainer.appendChild(tempMax);
+      tempMax.textContent = 'Max temperature: ' + day[i].main.temp_max + units;
+      intervalContainer.appendChild(tempMax);
       let tempMin = document.createElement('p');
       tempMin.classList.add('temperature');
-      tempMin.textContent = 'Min temperature: ' + interval.main.temp_min + units;
-      dayContainer.appendChild(tempMin);
+      tempMin.textContent = 'Min temperature: ' + day[i].main.temp_min + units;
+      intervalContainer.appendChild(tempMin);
       let weatherDesc = document.createElement('p');
-      weatherDesc.textContent = 'Weather: ' + interval.weather[0].description;
-      dayContainer.appendChild(weatherDesc);
+      weatherDesc.textContent = 'Weather: ' + day[i].weather[0].description;
+      intervalContainer.appendChild(weatherDesc);
+      if(i % 2 == 0) {
+       intervalContainer.style.gridColumnStart = '1';
+       intervalContainer.style.gridColumnEnd = '2'; 
+      } else {
+        intervalContainer.style.gridColumnStart = '2';
+        intervalContainer.style.gridColumnEnd = '3';
+      }
     }
     document.querySelector('.weather-container').appendChild(dayContainer);
   }
