bdb2c83f199374db3e446e4c304020be2954ae2a
Miriael <ravafiel@protonmail.com>
2023-02-23 16:00:48 +0100

    Add missing https

--- modules/api.js
+++ modules/api.js
@@ -6,14 +6,14 @@ import { stateDropdown, countryDropdown, cityInput, celsiusBox, fahrenheitBox } 
 async function getLocation(city, countryCode, stateCode) {
   if(countryCode == 'US') {
     try {
-      let url = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},${stateCode},${countryCode}&limit=1&appid=6538885ee061064bc99332441ad15f2b`);
+      let url = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city},${stateCode},${countryCode}&limit=1&appid=6538885ee061064bc99332441ad15f2b`);
       let result = await url.json();
       return [result[0].lat, result[0].lon];
     } catch (err) {
       alert(err);
     };
   }else try {
-      let url = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},${countryCode}&limit=1&appid=6538885ee061064bc99332441ad15f2b`);
+      let url = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city},${countryCode}&limit=1&appid=6538885ee061064bc99332441ad15f2b`);
       let result = await url.json();
       return [result[0].lat, result[0].lon];
     } catch (err) {
